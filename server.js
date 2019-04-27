9c25be8bf68511d12c755aa25a9d4c4ed502495f
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-04-26 23:38:00 -0500

    Scraped fixed.

--- server.js
+++ server.js
@@ -36,25 +36,34 @@ mongoose.connect(MONGODB_URI);
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
+
+  
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.npr.org/sections/world/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    //$("article div div h2").each(function(i, element) {
+    $("div.item-info ").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
+        .children("h2")
         .children("a")
         .text();
       result.link = $(this)
+        .children("p")
         .children("a")
         .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
+      result.summary = $(this)
+        .children("p")
+        .children("a")
+        .text();
+      
+        // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
           // View the added result in the console
